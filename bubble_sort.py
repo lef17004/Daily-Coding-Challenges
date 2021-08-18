8bc39fd4e3ac099ec7b2fa98b13264a4f5071ec7
Michael <lef17004@byui.edu>
2021-08-17 19:15:52 -0600

    Bubble sort solved

---
+++ bubble_sort.py
@@ -0,0 +1,52 @@
+
+###############################################################################
+"""
+Bubble Sort
+8/17/2021
+
+Time Complexity: O(n^2)
+
+Description: Starting from the beginning of the list, compare every adjacent
+pair, swap their position if they are not in the right order (the latter one is 
+smaller than the former one). After each iteration, one less element (the last 
+one) is needed to be compared until there are no more elements left to be 
+compared (Wikipedia).
+"""
+###############################################################################
+
+def main():
+    list = [64, 34, 25, 12, 22, 11, 90]
+    
+    bubble_sort(list)
+    print(list)
+    
+    
+    
+def bubble_sort(list):
+    # Loop through every element in the array (skip the last one).
+    for pass_number in range(0, len(list) - 1):
+        # Go from the beggining of the list to the end.
+        # Swap elements if the item before is greater than the one after it. 
+        for index in range(0, len(list) - 1 - pass_number):
+            if list[index] > list[index + 1]:
+                  swap(index, index + 1, list)
+            
+                        
+def swap(index1, index2, list):
+    # This swaps the elements in the list at the given idexes. 
+    list[index1], list[index2] = list[index2], list[index1]
+    
+    
+main()
+
+
+
+
+
+
+
+
+
+
+
+
